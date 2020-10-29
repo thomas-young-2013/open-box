2601ff70198237c1c5514399ee641ac3fd345131
thomas-young-2013 <1225646303@qq.com>
2020-10-29 19:23:38 +0800

    update master

--- test/optimizer/distributed/local_parallel_process.py
+++ test/optimizer/distributed/local_parallel_process.py
@@ -1,27 +1,29 @@
+import os
+import sys
 import logging
-logging.basicConfig(level=logging.INFO)
-
 import argparse
+sys.path.append(os.getcwd())
 import litebo.core.distributed.nameserver as hpns
 from litebo.optimizer.distributed_smbo import DistributedSMBO
 from litebo.examples.worker_example import BraninWorker
 
+logging.basicConfig(level=logging.INFO)
 parser = argparse.ArgumentParser(description='Local and Parallel Execution.')
 parser.add_argument('--n_iterations', type=int, help='Number of iterations performed by the optimizer', default=4)
 parser.add_argument('--n_workers', type=int, help='Number of workers to run in parallel.', default=2)
-parser.add_argument('--worker', help='Flag to turn this into a worker process', action='store_true')
+parser.add_argument('--mode', type=str, choices=['worker', 'master'], default='master')
 args = parser.parse_args()
 
 
 task_id = 'local_parallel_example'
 
-if args.worker:
+if args.mode == 'worker':
     w = BraninWorker(sleep_interval=0.5, nameserver='127.0.0.1', run_id=task_id)
     w.run(background=False)
     exit(0)
 
 # Start a nameserver.
-NS = hpns.NameServer(run_id='example3', host='127.0.0.1', port=9008)
+NS = hpns.NameServer(run_id='example3', host='127.0.0.1', port=None)
 NS.start()
 
 dsmbo = DistributedSMBO(task_id=task_id, config_space=BraninWorker.get_configspace())
