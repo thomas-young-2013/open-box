2c487876389a8473529cb77fe9761885cc977679
Yuanwei <yw.chen.math@gmail.com>
2021-02-06 15:28:41 +0800

    update README

--- test/optimizer/test_smbo.py
+++ test/optimizer/test_smbo.py
@@ -1,12 +1,4 @@
-import os
-import sys
 import numpy as np
-
-from ConfigSpace.hyperparameters import UniformFloatHyperparameter
-
-sys.path.append(os.getcwd())
-from litebo.optimizer.generic_smbo import SMBO
-from litebo.utils.config_space import ConfigurationSpace
 from litebo.utils.start_smbo import create_smbo
 
 
@@ -23,11 +15,6 @@ def branin(x):
     ret = a * (x2 - b * x1 ** 2 + c * x1 - r) ** 2 + s * (1 - t) * np.cos(x1) + s
     return {'objs': (ret,)}
 
-
-cs = ConfigurationSpace()
-x1 = UniformFloatHyperparameter("x1", -5, 10, default_value=0)
-x2 = UniformFloatHyperparameter("x2", 0, 15, default_value=0)
-cs.add_hyperparameters([x1, x2])
 
 config_dict = {
     "optimizer": "SMBO",
@@ -49,7 +36,6 @@ config_dict = {
     "task_id": 'hp1'
 }
 
-# bo = SMBO(branin, cs, advisor_type='default', max_runs=50, time_limit_per_trial=3, task_id='hp1')
 bo = create_smbo(branin, **config_dict)
 bo.run()
 inc_value = bo.get_incumbent()
