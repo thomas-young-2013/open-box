a0c7dea5db2321345115749372d33495297eae8a
thomas-young-2013 <1225646303@qq.com>
2021-03-22 23:45:11 +0800

    update doc and setup.

--- litebo/pkginfo.py
+++ litebo/pkginfo.py
@@ -1,8 +1,5 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .pkginfo import version as __version__, package_name
-
-__all__ = [
-    "__version__", "__package__", "package_name"
-]
+version = "0.6.1"  # Set by CI upon deploy
+package_name = "lite-bo"
