4d817f678710f4b55f0983e833742cddf4a319a0
thomas-young-2013 <1225646303@qq.com>
2021-03-25 15:50:05 +0800

    add contributing .

--- litebo/pkginfo.py
+++ litebo/pkginfo.py
@@ -1,5 +1,5 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-version = "0.6.2"  # Set by CI upon deploy
+version = "0.6.3"  # Set by CI upon deploy
 package_name = "lite-bo"
