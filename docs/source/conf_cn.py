5faab95c2bf5026ca4868ee91667c0a1707303c3
thomas-young-2013 <1225646303@qq.com>
2021-05-23 17:51:58 +0800

    test readthedocs

---
+++ docs/source/conf_cn.py
@@ -0,0 +1,93 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import sphinx_rtd_theme
+from recommonmark.parser import CommonMarkParser
+from recommonmark.transform import AutoStructify
+
+sys.path.insert(0, os.path.abspath('./../../'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'OpenBox'
+copyright = '2021, Thomas (Yang) Li'
+author = 'Thomas (Yang) Li'
+
+# The full version, including alpha/beta/rc tags
+release = 'beta'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['recommonmark', 'sphinx.ext.autodoc',
+              'sphinx.ext.doctest',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.napoleon']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
+language = None
+master_doc = 'zh_CN/index'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    'logo_only': True,
+}
+html_logo = '../logos/logo.png'
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/docs/'
+
+
+# app setup hook
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'url_resolver': lambda url: github_doc_root + url,
+        'auto_toc_tree_section': 'Contents',
+        'enable_math': False,
+        'enable_inline_math': False,
+        'enable_eval_rst': True
+    }, True)
+    app.add_transform(AutoStructify)
---
+++ docs/source/zh_CN/index.rst
@@ -0,0 +1,40 @@
+.. OpenBox documentation master file, created by
+   sphinx-quickstart on Fri Dec 23 13:31:47 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+:github_url: https://github.com/thomas-young-2013/open-box
+
+OpenBox: 高效通用的黑盒优化系统
+================================================================
+
+**OpenBox** is an open-source system designed for black-box
+optimization. Based on Bayesian optimization, OpenBox can solve
+black-box optimization problems efficiently. It supports not only
+traditional single objective black-box optimization problems (e.g.,
+hyperparameter optimization), but also multi-objective optimization,
+optimization with constraints, multiple parameter types, transfer
+learning, distributed parallel evaluation, multi-fidelity optimization,
+etc. Moreover, OpenBox provides both standalone local usage and online
+optimization service. Users can monitor and manage the optimization
+tasks on web pages, and also deploy their private optimization service.
+
+----------------------------------------
+
+Contents
+--------
+
+..  toctree::
+    :caption: Table of Contents
+    :maxdepth: 2
+    :titlesonly:
+
+    概览 <zh_CN/overview>
+
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
