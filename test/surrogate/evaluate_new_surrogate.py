ac3875d84b60ec7d552550172844a9a6406f68a7
thomas-young-2013 <1225646303@qq.com>
2021-06-17 18:26:08 +0800

    update lightgbm surrogate hyper-parameter.

--- test/surrogate/evaluate_new_surrogate.py
+++ test/surrogate/evaluate_new_surrogate.py
@@ -1,5 +1,10 @@
+import argparse
 from openbox.benchmark.objective_functions.synthetic import Branin
 from openbox.optimizer.generic_smbo import SMBO
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--surrogate', type=str, default='gp', choices=['gp', 'gp_mcmc', 'prf', 'lightgbm', 'tpe'])
+args = parser.parse_args()
 
 branin = Branin()
 bo = SMBO(branin.evaluate,      # objective function
@@ -7,7 +12,7 @@ bo = SMBO(branin.evaluate,      # objective function
           num_objs=branin.num_objs,  # number of objectives
           num_constraints=branin.num_constraints,  # number of constraints
           max_runs=50,          # number of optimization rounds
-          surrogate_type='prf',
+          surrogate_type=args.surrogate,
           time_limit_per_trial=180,
           task_id='quick_start')
 history = bo.run()
